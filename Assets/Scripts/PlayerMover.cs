41d486547beef20b5f7313c6e1eb921443c531d2
Hyhabuga <hyhabuga.games@gmail.com>
2024-08-04 19:50:20 +0300

    1.10
    10 урок

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -3,18 +3,20 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMover : MonoBehaviour
 {
+    private const float _SpeedCoefficient = 50;
+    
     [SerializeField] private float _speedX = 1;
 
     private Rigidbody2D _rigidbody2D;
 
-    void Start()
+    private void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
-        _rigidbody2D.velocity = new Vector2(_speedX, _rigidbody2D.velocity.y);
+        _rigidbody2D.velocity = new Vector2(_speedX * _SpeedCoefficient * Time.fixedDeltaTime, _rigidbody2D.velocity.y);
     }
 
 }
